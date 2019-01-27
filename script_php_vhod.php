74fef60e8d07164acd3341e219badb0aeb5b1c3f
DianaLysenko <42473300+Liss091@users.noreply.github.com>
2019-01-27 17:01:57 +0200

    website

---
+++ script_php_vhod.php
@@ -0,0 +1,21 @@
+﻿<?php
+	$email = $_POST['email'];
+    if (empty($email))
+	{
+		echo "Не введена почта"."<br>";
+	}
+	else
+	{
+		echo "Электронная почта "."$email"."<br>";
+	}
+///////////////////////////////////////////////////////
+	$passw = $_POST['passw1'];
+	if (empty($passw))
+	{
+		echo "Не введен пароль"."<br>";
+	}
+	else
+	{
+		echo "Пароль "."$passw"."<br>";
+	}
+?>
